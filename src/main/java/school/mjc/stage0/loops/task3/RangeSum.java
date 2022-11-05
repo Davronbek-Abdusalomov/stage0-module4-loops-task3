e7e247c0372afcfca43b7ed95ea5a7b1d4589bf0
Davronbek-Abdusalomov <2110117@newuzbekistanuniversity.uz>
2022-11-05 22:57:12 +0500

    for

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -3,9 +3,14 @@ package school.mjc.stage0.loops.task3;
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
         int sum =0;
-        for (int i=firstBoarder+1; i<secondBoarder-1; i++){
+        if (firstBoarder==secondBoarder){
+            System.out.println(firstBoarder);
+        }
+        else {
+        for (int i=firstBoarder; i<secondBoarder+1; i++){
             sum=sum+i;
             System.out.println(sum);
+         }
         }
     }
 }
