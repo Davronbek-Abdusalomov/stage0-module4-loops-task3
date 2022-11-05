919f33482b91304f742884423ee8815e33b3428f
Davronbek-Abdusalomov <2110117@newuzbekistanuniversity.uz>
2022-11-05 23:29:02 +0500

    for

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
-        System.out.println();
+        for (char i='a'; i<='z'; i++){
+            System.out.println(i);
+        }
     }
 }
