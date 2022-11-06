9266a4d202fdd63398a0c53ab285fab2d4031718
Davronbek-Abdusalomov <2110117@newuzbekistanuniversity.uz>
2022-11-06 15:25:11 +0500

    for

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        String s = String.valueOf(t);
+        String s = String.valueOf(t);
+        int sum=0;
+        for (int i=0; i<s.length(); i++){
 
+            sum = sum + Integer.parseInt(String.valueOf(s.charAt(i)));
+
+        }
+        System.out.println(sum);
     }
 }
