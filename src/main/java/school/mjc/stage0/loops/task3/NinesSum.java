f9825d85d99f8160ce1b33c6e84bd5dc8386b8a5
Davronbek-Abdusalomov <2110117@newuzbekistanuniversity.uz>
2022-11-06 15:37:17 +0500

    for

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        for (int i = 0; i<lengthOfLastNumber; i++){
+           sum=sum+(sum*10+9);
+        }
+        System.out.println(sum);
     }
 }
